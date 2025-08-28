e61e8734bab74fd1cb4e0e492bcb3431ee91d027
Eduarda Silva <mariaedu_irt15@icloud.com>
2025-08-28 15:49:24 -0300

    subindo códigos

---
+++ Códigos Eduarda/Meu primeiro código.py
@@ -0,0 +1,4 @@
+nome = input("qual seu nome?")
+print(nome)
+idade = input("qual a sua idade?")
+print (idade)
---
+++ Códigos Eduarda/Meu segundo programa.py
@@ -0,0 +1,18 @@
+
+
+#Exercicio 2
+#crie uma váriavel que se chame nome_carro, coloque dentro dessa váriavel o nome do seu carro dos sonhos
+
+
+nome = input ("digite o nome do carro:")
+print(nome)
+
+
+#crie uma váriavel que se chame preço, coloque dentro dessa váriavel o preço do carro
+preço = float (input("digite o preço:"))
+print(preço)
+
+
+#crie uma váriavel que se chame ano, coloque dentro dessa váriavelo ano do carro
+ano = int(input("digite o ano do carro:"))
+print(ano)
