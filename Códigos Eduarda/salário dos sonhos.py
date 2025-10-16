4855f8daf3a4a64ebfa32ac0c6623eb79b693fc9
Eduarda Silva <mariaedu_irt15@icloud.com>
2025-10-16 15:08:08 -0300

    Add files via upload

---
+++ Códigos Eduarda/salário dos sonhos.py
@@ -0,0 +1,15 @@
+salario = 40000.00
+casa = 8500.00
+alimentacao = 2000.00
+carro = 2500.00
+animais_domesticos = 800.00
+viagens = 5000.00
+lazer = 2000
+academia = 200.00
+roupa = 4000
+calçado = 2000
+cabelo = 3000
+cursos = 3000
+total_gastos = casa + alimentacao + carro + animais_domesticos + viagens + lazer + cursos
+restante = salario - total_gastos
+print((f"Sobram R$ {restante:.2f} do salário dos sonhos"))
